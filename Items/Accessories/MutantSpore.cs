2fa299916cf9ee7a7d139997de134b7d3983f8ac
Kronoshifter <Kronoshifter@gmail.com>
2020-07-07 12:03:57 -0600

    added mutant spore code

---
+++ Items/Accessories/MutantSpore.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kronos.Items.Accessories
+{
+    public class MutantSpore : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Composed of matter, anti-matter, and it doesn't matter");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.value = 100;
+            item.rare = ItemRarityID.Orange;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.maxMinions += 1;
+            
+        }
+    }
+}
