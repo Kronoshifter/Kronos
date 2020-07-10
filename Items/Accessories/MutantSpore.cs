21ff98b3dcf6b75fbc11fcc1471f3fdf1d0c85f3
Kronoshifter <Kronoshifter@gmail.com>
2020-07-10 05:15:51 -0600

    Added placeholder images Mutant spores now drop from jungle spores with a 4% chance

--- Items/Accessories/MutantSpore.cs
+++ Items/Accessories/MutantSpore.cs
@@ -15,11 +15,12 @@ namespace Kronos.Items.Accessories
 
         public override void SetDefaults()
         {
-            item.width = 20;
-            item.height = 20;
+            item.width = 30;
+            item.height = 26;
             item.value = 100;
             item.rare = ItemRarityID.Orange;
             item.accessory = true;
+            item.scale = 0.85f;
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
---
+++ KronosGlobalTile.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kronos
+{
+    public class KronosGlobalTile : GlobalTile
+    {
+        public override bool Drop(int i, int j, int type)
+        {
+            // Mutant Spore
+            if (type == TileID.JunglePlants && Main.rand.NextBool(25))
+            {
+                Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("MutantSpore"));
+            }
+
+            return true;
+        }
+    }
+}
