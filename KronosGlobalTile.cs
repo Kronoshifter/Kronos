7a23936106d30ad16a2be27effb4dd9a80124144
Kronoshifter <Kronoshifter@gmail.com>
2020-07-12 23:30:08 -0600

    converted global tile statements to switch

--- KronosGlobalTile.cs
+++ KronosGlobalTile.cs
@@ -9,10 +9,15 @@ namespace Kronos
     {
         public override bool Drop(int i, int j, int type)
         {
-            // Mutant Spore
-            if (type == TileID.JunglePlants && Main.rand.NextBool(25))
+            switch (type)
             {
-                Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("MutantSpore"));
+                // Mutant Spore
+                case TileID.JunglePlants when Main.rand.NextBool(25):
+                    Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("MutantSpore"));
+                    break;
+                // Shadow Ring
+                case TileID.ShadowOrbs:
+                    break;
             }
 
             return true;
