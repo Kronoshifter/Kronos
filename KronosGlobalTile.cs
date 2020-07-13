f56bcc1208ee323b9d7b3e420d34a6458cd967fb
Kronoshifter <Kronoshifter@gmail.com>
2020-07-13 01:16:55 -0600

    Added spawn chance for Crimson Amulet and Shadow Ring

--- KronosGlobalTile.cs
+++ KronosGlobalTile.cs
@@ -9,14 +9,24 @@ namespace Kronos
     {
         public override bool Drop(int i, int j, int type)
         {
+            var modPlayer = Main.LocalPlayer.GetModPlayer<KronosPlayer>();
+            
             switch (type)
             {
                 // Mutant Spore
                 case TileID.JunglePlants when Main.rand.NextBool(25):
                     Item.NewItem(i * 16, j * 16, 16, 16, mod.ItemType("MutantSpore"));
                     break;
-                // Shadow Ring
+                // Shadow Ring & Crimson Amulet
                 case TileID.ShadowOrbs:
+                    // spawn chance, should preserve normal drops
+                    if (Main.rand.NextBool(1, 6))
+                    {
+                        int itemToSpawn = mod.ItemType(WorldGen.crimson ? "CrimsonAmulet" : "ShadowRing");
+                        Item.NewItem(i * 16, j * 16, 16, 16, itemToSpawn);
+                        return false;
+                    }
+                    
                     break;
             }
 
